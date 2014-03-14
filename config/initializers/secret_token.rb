3999222c6f3bb23670baf604dfdedf1342fab1bd
dyep49 <dyep49@gmail.com>
2014-03-14 19:18:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alex::Application.config.secret_key_base = '37a90403f61f1cddd72136223c6d462a84eccc9bd5056cfa0898b962a32a14feb9d9cfa096c6e2c476c2a737927d39a2538de96185230f0b8cd101c6e47efa54'
