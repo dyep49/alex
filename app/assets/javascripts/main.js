7a42010a1d8e845a3c96dcf2fcb7829a262e3ffa
zumbalogy <intrapostal@gmail.com>
2014-03-16 16:46:28 -0400

    some angular file seperation of concerns

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,19 +1,27 @@
-var main = angular.module('main', ['ngRoute'])
+var main = angular.module('main', ['ngRoute', 'mainControllers'])
 
 main.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/', {
-                templateUrl: 'test.html',
-                controller: 'test'
+                templateUrl: 'splash.html',
+                controller: 'splash'
+            }).
+            when('/tag/:tag_name', {
+                templateUrl: 'tag.html',
+                controller: 'tag'
+            }).
+            when('/site/:source_name', {
+                templateUrl: 'site.html',
+                controller: 'site'
+            }).
+            when('/pin/:pin_id', {
+                templateUrl: 'pin.html',
+                controller: 'pin'
             }).
             otherwise({
                 redirectTo: '/'
             });
     }
 ]);
-
-main.controller('test', function($scope) {
-  $scope.array = [1,2,3, "sdfds"];
-});
 
---
+++ app/assets/javascripts/mainControllers.js
@@ -0,0 +1,8 @@
+
+var mainControllers = angular.module('mainControllers', [])
+
+
+mainControllers.controller('splash', function($scope) {
+  $scope.array = [1,2,3, "sdfds"];
+});
+
