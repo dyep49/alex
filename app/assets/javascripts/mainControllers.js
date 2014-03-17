eea4c750657540186aad29be4d413ee11b6f7d7c
zumbalogy <intrapostal@gmail.com>
2014-03-16 22:18:52 -0400

    got angular route params access figured out

--- app/assets/javascripts/mainControllers.js
+++ app/assets/javascripts/mainControllers.js
@@ -2,16 +2,16 @@ var mainControllers = angular.module('mainControllers', [])
 
 
 mainControllers.controller('splash', function($scope) {
-    $scope.array = [1,2,3, "sdfds"]
+    $scope.array = ['a',1,2,3, "sdfds"]
 });
 
 mainControllers.controller('site', function($scope) {
     $scope.array = [] //ajax to get all pins for a site
 });
 
-mainControllers.controller('tag', function($scope) {
-    $scope.array = [] //ajax to get all pins of a tag
-});
+mainControllers.controller('tag', ['$scope','$routeParams', function($scope, $routeParams) {
+    $scope.array = ['test', $routeParams] //ajax to get all pins of a tag
+}]);
 
 mainControllers.controller('pin', function($scope) {
     $scope.array = [] // ajax to get a pin (realize we would already have it down
