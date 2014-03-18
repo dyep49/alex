aa55195aa9fe0be8f14e53bd90ccb7bbf0bc849b
zumbalogy <intrapostal@gmail.com>
2014-03-18 19:10:39 -0400

    routeparams mystery

--- app/assets/javascripts/angular/controllers/pin.controller.js
+++ app/assets/javascripts/angular/controllers/pin.controller.js
@@ -1,4 +1,5 @@
 main.controller('PinController', ['$scope', '$http', '$routeParams', 'Pin', function($scope, $routeParams, $http, Pin){
+    
 	$scope.pin = Pin.show($routeParams.pin_id)
 
     $scope.savePin = function(){
