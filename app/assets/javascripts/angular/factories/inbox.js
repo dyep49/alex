158eb271ffe72460681685be78d633b727c53230
zumbalogy <intrapostal@gmail.com>
2014-03-23 14:08:55 -0400

    clean

--- app/assets/javascripts/angular/factories/inbox.js
+++ app/assets/javascripts/angular/factories/inbox.js
@@ -6,15 +6,15 @@ main.service('Inbox', ['$http', function($http){
 
 	this.fetchCount = function(){
 		$http({
-		url: 'get_inbox',
-		method: 'GET', 
-	})
+			url: 'get_inbox',
+			method: 'GET', 
+		})
 		.success(function(data){
-	    for (var i = 0; i < data.length; i++){
-	        if (data[i].seen === false){
-	            unseen_shares.push(data[i])
-	        } 
-	    }
+		    for (var i = 0; i < data.length; i++){
+		        if (data[i].seen === false){
+		            unseen_shares.push(data[i])
+		        } 
+		    }
 		})
 	}
 
@@ -23,19 +23,18 @@ main.service('Inbox', ['$http', function($http){
 	}
 
 	this.fetchUser = function(id){
-			$http({
-				url: '/find_user',
-				method: 'GET',
-				params: {user_id: id}
-			})
-				.success(function(response){
-					users.push(response);
-				})		
+		$http({
+			url: '/find_user',
+			method: 'GET',
+			params: {user_id: id}
+		})
+		.success(function(response){
+			users.push(response);
+		})		
 	}
 
 	this.getUsers = function(){
 		return users
 	}
-
 
 }])
