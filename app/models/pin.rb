0e18dc95c86afe5baa40b7950caffc7b6dbfa083
zumbalogy <intrapostal@gmail.com>
2014-03-23 20:46:56 -0400

    super bubble tags on new pin page, voalla

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -22,8 +22,8 @@ class Pin < ActiveRecord::Base
   end
 
   def make_tags(pin, tags)
-    tags.gsub!(',','')
-    tags.split.each do |word|
+    tags.gsub!(' ','')
+    tags.split(',').each do |word|
       tag = Tag.find_by_name(word.downcase)
       pin.tags << (tag || Tag.create(name: word.downcase))
     end
