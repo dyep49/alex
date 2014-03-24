87fb058d81dfa82a56c9ab190cd2b4df104ce7a1
dyep49 <dyep49@gmail.com>
2014-03-24 02:47:42 -0400

    shares show up on user dash

--- app/assets/javascripts/angular/controllers/dash.controller.js
+++ app/assets/javascripts/angular/controllers/dash.controller.js
@@ -1,13 +1,33 @@
-main.controller('DashController', ['$scope', 'Dash', function($scope, Dash){
+main.controller('DashController', ['$scope', 'Dash', 'Inbox', 'Pin', function($scope, Dash, Inbox, Pin){
 
     function init(){
         Dash.getSavedArray()
         Dash.getHistoryArray()
+        setTimeout(function(){
+	        if ($scope.shares.length > 0){
+				$scope.shares.forEach(function(share){
+					var username = share.from_user_name
+					var object = {}
+					object["username"] = username
+					object["pin"] = Pin.show(share.pin_id)
+					$scope.pins.push(object);
+					// Inbox.fetchUser(share.from_user_id);
+				})
+			}
+        }, 0)
+
     }
     
     init()
 
     $scope.savedArray = Dash.savedArray()
     $scope.historyArray = Dash.historyArray()
+
+
+	$scope.shares = Inbox.getCount();
+	$scope.share_users = Inbox.getUsers();
+	$scope.pins = []
+
+
 
 }])
