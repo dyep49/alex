16784df9c14ad815834cb1a442606b5862815e60
zumbalogy <intrapostal@gmail.com>
2014-03-23 20:05:29 -0400

    stuff for column and new pin html

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -22,9 +22,10 @@ class Pin < ActiveRecord::Base
   end
 
   def make_tags(pin, tags)
+    tags.gsub!(',','')
     tags.split.each do |word|
-      tag = Tag.find_by_name(word)
-      pin.tags << (tag || Tag.create(name: word))
+      tag = Tag.find_by_name(word.downcase)
+      pin.tags << (tag || Tag.create(name: word.downcase))
     end
   end
 
