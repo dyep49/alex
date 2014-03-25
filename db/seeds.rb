779ff50418f431e1e4ca544751a65b23c1ffe9eb
zumbalogy <intrapostal@gmail.com>
2014-03-25 15:39:24 -0400

    seed sources fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,31 @@ tag_array.each do |name|
     tag.save
 end
 
-techcrunch = Source.create(url: 'www.techcrunch.com', img_url: 'https://s3.amazonaws.com/crunchbase_prod_assets/assets/images/resized/0001/0576/10576v3-max-250x250.jpg', name: 'Techcrunch')
-Source.create(url: 'www.soundcloud.com', img_url: 'http://cdn.embed.ly/providers/logos/soundcloud.png', name: 'Soundcloud')
-Source.create(url: 'www.youtube.com', img_url: 'https://lh6.ggpht.com/NrQdFAdPSI9-hreB4C7HNhj3yXRiW1jqOOi7eFyakIx_IA-Im0huIeYCs5jTidMT2qA=w300', name: 'YouTube')
-Source.create(url: 'www.vimeo.com', img_url: 'http://www.cmha.bc.ca/files/vimeo-icon.png', name: 'Vimeo')
-Source.create(url: 'www.espn.com', img_url: 'https://yt3.ggpht.com/-yOoKXdob9y8/AAAAAAAAAAI/AAAAAAAAAAA/O9ATiv9wGJ0/s900-c-k-no/photo.jpg', name: 'ESPN')
+techcrunch = Source.create(
+    url: 'www.techcrunch.com',
+    img_url: 'https://s3.amazonaws.com/crunchbase_prod_assets/assets/images/resized/0001/0576/10576v3-max-250x250.jpg',
+    name: 'Techcrunch'
+)
+soundcloud = Source.create(
+    url: 'www.soundcloud.com',
+    img_url: 'http://cdn.embed.ly/providers/logos/soundcloud.png',
+    name: 'Soundcloud'
+)
+youtube = Source.create(
+    url: 'www.youtube.com',
+    img_url: 'https://lh6.ggpht.com/NrQdFAdPSI9-hreB4C7HNhj3yXRiW1jqOOi7eFyakIx_IA-Im0huIeYCs5jTidMT2qA=w300',
+    name: 'YouTube'
+)
+Source.create(
+    url: 'www.vimeo.com',
+    img_url: 'http://www.cmha.bc.ca/files/vimeo-icon.png',
+    name: 'Vimeo'
+)
+Source.create(
+    url: 'www.espn.com',
+    img_url: 'https://yt3.ggpht.com/-yOoKXdob9y8/AAAAAAAAAAI/AAAAAAAAAAA/O9ATiv9wGJ0/s900-c-k-no/photo.jpg',
+    name: 'ESPN'
+)
 
 
 
@@ -33,7 +53,7 @@ end
         description: "Ta-ku has been consistanly at the top of this game for like 2 years now. Such solid, distictive in style yet unique per song, good solo and just one off remixes. and the sets are good. Like a diplo more in touch with the art. The JT of this electornica game.",
         image_url: 'http://soundisstyle.com/wp-content/uploads/2013/04/Flume-Left-Alone-Ta-Ku-Remix-612x612.jpg'
     ) 
-    techcrunch.pins << pin
+    soundcloud.pins << pin
     pin.tags << Tag.find_by_name('music')
 end
 
@@ -44,7 +64,7 @@ super_view = Pin.create(
         image_url: 'http://cdn.hiphopdx.org//images/news/2-Kendrick-Lamar-Eminem.jpg',
         view_count: 1_000_000
 ) 
-
+soundcloud.pins << super_view
 super_view.tags << Tag.find_by_name('music')
 
 
@@ -55,6 +75,7 @@ youtube_pin = Pin.create(
         image_url: 'http://images.dazedcdn.com/786/dd/730/3/733061.jpg'
 ) 
 
+youtube.pins << youtube_pin
 youtube_pin.tags << Tag.find_by_name('music')
 youtube_pin.tags << Tag.find_by_name('video')
 
