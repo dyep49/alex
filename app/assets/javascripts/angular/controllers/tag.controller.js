83a07a4cef4c5e542b744349b0f6b93af1d18465
zumbalogy <intrapostal@gmail.com>
2014-03-29 14:54:42 -0400

    probably better way to compare times, but time sorting in for infinity scroll done

--- app/assets/javascripts/angular/controllers/tag.controller.js
+++ app/assets/javascripts/angular/controllers/tag.controller.js
@@ -15,16 +15,45 @@ main.controller('TagController', ['$scope', '$routeParams', 'Tag', function($sco
     }
 
     $scope.mostRecent = function(){
+        $scope.array = $scope.save
         $scope.sortBy = 'created_at'
     }
     $scope.mostViewed = function(){
+        $scope.array = $scope.save
+        $scope.sortBy = 'view_count'
+    }
+
+    $scope.mostMonth = function(){
+        var new_array = []
+        var currentDate = new Date();
+        for (var i = 0; i < $scope.save.length; i ++) {
+            var oldDate = new Date($scope.save[i].created_at)
+            if ((oldDate - currentDate) < 18144000000){
+                new_array.push($scope.save[i])
+            }
+        }
+        $scope.array = new_array
+        $scope.sortBy = 'view_count'
+    }
+
+    $scope.mostWeek = function(){
+        var new_array = []
+        var currentDate = new Date();
+        for (var i = 0; i < $scope.save.length; i ++) {
+            var oldDate = new Date($scope.save[i].created_at)
+            if ((oldDate - currentDate) < 604800000){
+                new_array.push($scope.save[i])
+            }
+        }
+        $scope.array = new_array
         $scope.sortBy = 'view_count'
     }
     
 
     init()
 
-    $scope.array = Tag.getStuff()
+    $scope.save = Tag.getStuff()
+    $scope.array = $scope.save
 
 }])
 
