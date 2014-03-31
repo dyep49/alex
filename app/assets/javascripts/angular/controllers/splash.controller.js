6d7ca1edd36da55316e570ccf9597fee12caee95
dyep49 <dyep49@gmail.com>
2014-03-31 18:02:29 -0400

    bxslider and asset stuff

--- app/assets/javascripts/angular/controllers/splash.controller.js
+++ app/assets/javascripts/angular/controllers/splash.controller.js
@@ -1,3 +1,6 @@
 main.controller('SplashController', ['$scope', 'Splash', function($scope, Splash) {
     $scope.array = Splash.all()
+
+    $scope.currentSlider = 0
+    $scope.total = 7
 }]);
--- app/assets/javascripts/angular/directives/bxslider.js
+++ app/assets/javascripts/angular/directives/bxslider.js
@@ -4,15 +4,9 @@ main.directive('bxSlider', [function () {
         link: function (scope, element, attrs) {
             scope.$on('repeatFinished', function () {
                 console.log("ngRepeat has finished");
+                scope.currentSlider ++
+                if(scope.currentSlider <= scope.total){
                 element.bxSlider(scope.$eval('{' + attrs.bxSlider + '}'));
-                for(i=1; i<$('.saves').find('.bx-next').length; i++){
-                    $('.saves').find('.bx-next').last().remove()
-                }
-                for(i=1; i<$('.history').find('.bx-next').length; i++){
-                    $('.history').find('.bx-next').last().remove()
-                }
-                for(i=1; i<$('.shares').find('.bx-next').length; i++){
-                    $('.shares').find('.bx-next').last().remove()
                 }
             });
         }
@@ -23,6 +17,7 @@ main.directive('bxSlider', [function () {
         restrict: 'A',
         link: function (scope, element, attr) {
             if (scope.$last === true) {
+                console.log('test')
                 $timeout(function () {
                     scope.$emit('repeatFinished');
                 });
@@ -30,3 +25,25 @@ main.directive('bxSlider', [function () {
         }
     }
 }]);
+
+
+// main.directive('bxSlider', [function () {
+//     return {
+//         restrict: 'A',
+//         link: function (scope, element, attrs) {
+//             scope.$on('repeatFinished', function () {
+//                 console.log("ngRepeat has finished");
+//                 element.bxSlider(scope.$eval('{' + attrs.bxSlider + '}'));
+//                 for(i=1; i<$('.saves').find('.bx-next').length; i++){
+//                     $('.saves').find('.bx-next').last().remove()
+//                 }
+//                 for(i=1; i<$('.history').find('.bx-next').length; i++){
+//                     $('.history').find('.bx-next').last().remove()
+//                 }
+//                 for(i=1; i<$('.shares').find('.bx-next').length; i++){
+//                     $('.shares').find('.bx-next').last().remove()
+//                 }
+//             });
+//         }
+//     }
+// }])
