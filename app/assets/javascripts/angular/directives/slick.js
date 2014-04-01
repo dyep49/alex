caf881885556e3f6dcba958c2b5b82b55319b71c
dyep49 <dyep49@gmail.com>
2014-04-01 19:50:16 -0400

    sliders work

---
+++ app/assets/javascripts/angular/directives/slick.js
@@ -0,0 +1,27 @@
+main.directive('slick', ['$timeout', function($timeout){
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs){
+			// scope.$on('repeatEnd', function(){
+			$timeout(function(){
+					console.log('last one');
+					$('.splash-wrapper').css('visibility', 'visible')
+					$('.splash-wrapper').addClass('splash-animation')
+					$(element).slick(scope.$eval('{' + attrs.slick + '}'))					
+			}, 500)
+			// })
+		}
+	}
+}])
+// .directive('notifyRepeatFinish', ['$timeout', function($timeout){
+// 	return {
+// 		restrict: 'A',
+// 		link: function(scope, element, attr){
+// 			if (scope.$last === true){
+// 				$timeout(function(){
+// 					scope.$emit('repeatEnd')
+// 				})
+// 			}
+// 		}
+// 	}
+// }])
